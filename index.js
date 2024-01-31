3a25762f92bef35c57b7b8c57b5bc9feb6353627
capeya <capeya92@gmail.com>
2024-01-31 18:52:39 +0100

    add some loop in the main

--- index.js
+++ index.js
@@ -52,44 +52,73 @@ function donnerUneCarte(jeuDeCarte) {
 }
 
 function compteDesCartes(cartesDuJoueur) {
-    return cartesDuJoueur.reduce((prev, curr) => prev + curr.valeur, 0);
+    return cartesDuJoueur.reduce((prev, curr) => prev + curr.point, 0);
 }
-
-function hasBlackjack(compteDesCartes) {
-    return compteDesCartes === 21
-}
-
-function beatTheDealer(compteDesCartesJoueur, compteDesCartesDealer) {
-    return compteDesCartesJoueur > compteDesCartesDealer;
-}
-
-//hasBlackjack(compteDesCartes(carteDuJoueur));
-//beatTheDealer(compteDesCartes(carteDuJoueur), compteDesCartes(carteDuDealer));
 
 function main() {
 
-    cartesDuJoueur = []
+    bankrollDuJoueur = 1000;
+
     const jeuDeCartes = nouveauJeuDeCartes();
+
     while (true) {
 
-        const decision = prompt("Tu veux une Carte ? (y/n)");
-        if (decision === "y") {
-            cartesDuJoueur.push(donnerUneCarte(jeuDeCartes))
+        cartesDuDealer = []
+        cartesDuJoueur = []
+
+        // Donner deux cartes au joueur
+        cartesDuJoueur.push(donnerUneCarte(jeuDeCartes));
+        cartesDuJoueur.push(donnerUneCarte(jeuDeCartes));
+        // Donner deux cartes au dealer (2eme dans la boucle)
+        cartesDuDealer.push(donnerUneCarte(jeuDeCartes));
+
+        while (true) {
+
+            if (compteDesCartes(cartesDuJoueur) > 21) {
+                break // PERDU
+            }
+
+            if (compteDesCartes(cartesDuDealer) < 17) {
+                cartesDuDealer.push(donnerUneCarte(jeuDeCartes));
+            }
+
+            alert(
+                "\nCompte Carte Joueur: " + compteDesCartes(cartesDuJoueur) +
+                "\nCompte Carte Dealer: " + compteDesCartes(cartesDuDealer)
+            )
+
+            const decision = prompt("Tu veux une carte ? (y/n)");
+            if (decision === "y") {
+                cartesDuJoueur.push(donnerUneCarte(jeuDeCartes))
+            }
+            else if (decision === "n") {
+                break; // Sortir du while
+            }
+            else {
+                console.log("WARNING! Command " + decision + " n'existe pas.")
+            }
+
         }
 
-        else if (decision === "n" || compteDesCartes(cartesDuJoueur) > 21) {
-            console.log("Bye bye");
+
+        if (compteDesCartes(cartesDuJoueur) > 21) {
+
+            alert("Mise Perdu")
+        }
+        else if (compteDesCartes(cartesDuJoueur) == 21) {
+            alert("Tu as gagné 1.5x ta mise !")
+        }
+        else if (compteDesCartes(cartesDuJoueur) > compteDesCartes(cartesDuDealer)) {
+            alert("Tu as gagné 1x ta mise !")
+        }
+        else {
+            alert("Mise Perdu")
+        }
+
+        const decision = prompt("Rejouer ? (y/n)");
+        if (decision == "n") {
             break;
         }
-
-        else {
-            console.log("WARNING! Command " + decision + "n'existe pas.")
-        }
-
-        console.log("Compte Carte Joueur: ", compteDesCartes(cartesDuJoueur));
-        console.log("carte reçu: ", cartesDuJoueur.at(-1))
-        console.log("nbr carte joueur: ", cartesDuJoueur.length)
-        console.log("nbr carte jeu de cartes: ", jeuDeCartes.length)
     }
 }
 
